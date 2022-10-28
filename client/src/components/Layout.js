489d88e4ab8ee2cdc8bf0114f7e1a8bd4057215a
DuBrowski <krdubray@comcast.net>
2022-10-28 11:21:30 -0500

    fix reroute to dashboard when valid user token present

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,6 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Layout = () => {
+  const auth = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.user) navigate("/dashboard");
+  }, [auth, navigate]);
+
   return <Outlet />;
 };
 
--- client/src/utilities/AuthOutlet.js
+++ client/src/utilities/AuthOutlet.js
@@ -1,15 +1,9 @@
-import { useEffect } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export const AuthOutlet = () => {
   const auth = useSelector((state) => state.auth);
   const location = useLocation();
-
-  useEffect(() => {
-    console.log(auth);
-  }, []);
-  console.log(auth);
 
   return auth.user ? (
     <Outlet />
