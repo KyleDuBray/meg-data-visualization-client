ca3dffef2fb3be002190483fc003ed3e5da49462
DuBrowski <krdubray@comcast.net>
2022-11-03 13:17:11 -0500

    Clean comments

--- client/src/hooks/useMenuToggler.js
+++ client/src/hooks/useMenuToggler.js
@@ -1,24 +1,18 @@
 import React, { useRef, useEffect, useState } from "react";
 
-/**
- * Hook that alerts clicks outside of the passed ref
- */
 function useMenuToggler(ref) {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    /**
-     * Alert if clicked on outside of element
-     */
+    /*https://stackoverflow.com/questions/27900053/css-transition-with-visibility-not-working
+    Alert if clicked on outside of element*/
     function handleClickOutside(event) {
       if (ref.current && !ref.current.contains(event.target)) {
         setIsOpen(false);
       }
     }
-    // Bind the event listener
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      // Unbind the event listener on clean up
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [ref]);
