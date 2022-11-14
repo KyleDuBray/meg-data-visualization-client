9ce0ffce0b67be05417be4a36a69592af4a4ee6b
DuBrowski <krdubray@comcast.net>
2022-11-14 15:08:22 -0600

    sidebar style

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,16 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
   return (
-    <div className="w-0 invisible md:visible md:w-full h-full flex ml-auto flex-col">
-      <Link className="text-white text-center" to="/dashboard/projects">
-        Projects
-      </Link>
-      <Link className="text-white text-center" to="/dashboard/data">
-        Data
-      </Link>
-    </div>
+    <ul className="w-0 invisible md:visible md:w-full h-full flex ml-auto flex-col">
+      <li
+        className={`${
+          location.pathname === "/dashboard/projects"
+            ? "border-r-4 border-gray-200"
+            : ""
+        } flex justify-center my-2 w-full`}
+      >
+        <Link
+          className={` text-gray-300 hover:bg-gray-700 hover:text-white font-medium text-center  md:mx-8 lg:mx-16 xl:mx-24 rounded`}
+          to="/dashboard/projects"
+        >
+          Projects
+        </Link>
+      </li>
+
+      <li
+        className={`${
+          location.pathname === "/dashboard/data"
+            ? "border-r-4 border-gray-200"
+            : ""
+        } flex justify-center my-2 w-full`}
+      >
+        <Link
+          className={` text-gray-300 hover:bg-gray-700 hover:text-white font-medium text-center  md:mx-8 lg:mx-16 xl:mx-24 rounded`}
+          to="/dashboard/data"
+        >
+          Data
+        </Link>
+      </li>
+    </ul>
   );
 };
 
