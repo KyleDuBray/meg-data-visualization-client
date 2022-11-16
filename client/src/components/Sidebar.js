787795c312f8784390e670ed78405fdeca2e6ef3
Kyle <krdubray@comcast.net>
2022-11-15 21:09:15 -0600

    sidebar link icons

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,5 +1,7 @@
-import React, { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { HiOutlineLightBulb } from 'react-icons/hi';
+import { CiWavePulse1 } from 'react-icons/ci';
 
 const Sidebar = () => {
   const location = useLocation();
@@ -12,32 +14,28 @@ const Sidebar = () => {
     <ul className="w-0 invisible md:visible md:w-full h-full flex ml-auto flex-col">
       <li
         className={`${
-          location.pathname === "/dashboard/projects"
-            ? "border-r-4 border-gray-200"
-            : ""
+          location.pathname === '/dashboard/projects'
+            ? 'border-r-4 border-gray-200'
+            : ''
         } flex justify-center my-2 w-full`}
       >
-        <Link
-          className={` text-gray-300 hover:bg-gray-700 hover:text-white font-medium text-center  md:mx-8 lg:mx-16 xl:mx-24 rounded`}
-          to="/dashboard/projects"
-        >
-          Projects
-        </Link>
+        <div className="flex text-gray-300 hover:bg-gray-700 hover:text-white font-medium text-center md:mx-8 lg:mx-16 xl:mx-24 rounded">
+          <HiOutlineLightBulb className="text-white h-6 w-6 mr-2" />
+          <Link to="/dashboard/projects">Projects</Link>
+        </div>
       </li>
 
       <li
         className={`${
-          location.pathname === "/dashboard/data"
-            ? "border-r-4 border-gray-200"
-            : ""
+          location.pathname === '/dashboard/data'
+            ? 'border-r-4 border-gray-200'
+            : ''
         } flex justify-center my-2 w-full`}
       >
-        <Link
-          className={` text-gray-300 hover:bg-gray-700 hover:text-white font-medium text-center  md:mx-8 lg:mx-16 xl:mx-24 rounded`}
-          to="/dashboard/data"
-        >
-          Data
-        </Link>
+        <div className="flex text-gray-300 hover:bg-gray-700 hover:text-white font-medium text-center md:mx-8 lg:mx-16 xl:mx-24 rounded">
+          <CiWavePulse1 className="text-white h-6 w-6 mr-2" />
+          <Link to="/dashboard/data">Data</Link>
+        </div>
       </li>
     </ul>
   );
