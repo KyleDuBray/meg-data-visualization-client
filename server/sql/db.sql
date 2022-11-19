60e43402bdad88a2d96cd71b613debdf011a4c0b
DuBrowski <krdubray@comcast.net>
2022-11-18 22:40:46 -0600

    add unique key combination for user_id and project_id in works_on table

--- server/sql/db.sql
+++ server/sql/db.sql
@@ -1,131 +1,123 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `meg` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `meg`;
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: meg
+-- ------------------------------------------------------
+-- Server version	8.0.30
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema meg
--- -----------------------------------------------------
+--
+-- Table structure for table `event`
+--
 
--- -----------------------------------------------------
--- Schema meg
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `meg` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `meg` ;
-
--- -----------------------------------------------------
--- Table `meg`.`project`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `meg`.`project` ;
-
-CREATE TABLE IF NOT EXISTS `meg`.`project` (
-  `project_id` INT NOT NULL AUTO_INCREMENT,
-  `project_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`project_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `meg`.`user`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `meg`.`user` ;
-
-CREATE TABLE IF NOT EXISTS `meg`.`user` (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(30) NOT NULL,
-  `last_name` VARCHAR(30) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `organization` VARCHAR(255) NULL DEFAULT NULL,
-  `password` CHAR(60) NOT NULL,
-  PRIMARY KEY (`user_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 15
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `meg`.`event`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `meg`.`event` ;
-
-CREATE TABLE IF NOT EXISTS `meg`.`event` (
-  `event_id` INT NOT NULL AUTO_INCREMENT,
-  `raw_event_data` LONGBLOB NOT NULL,
-  `event_type` VARCHAR(45) NULL DEFAULT NULL,
-  `project_id` INT NULL DEFAULT NULL,
-  `user_id` INT NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `event` (
+  `event_id` int NOT NULL AUTO_INCREMENT,
+  `raw_event_data` longblob NOT NULL,
+  `event_type` varchar(45) DEFAULT NULL,
+  `project_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
   PRIMARY KEY (`event_id`),
-  INDEX `fk_project_id_idx` (`project_id` ASC) VISIBLE,
-  INDEX `fk_project_id_event_idx` (`project_id` ASC) VISIBLE,
-  INDEX `fk_user_id_event_idx` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_project_id_event`
-    FOREIGN KEY (`project_id`)
-    REFERENCES `meg`.`project` (`project_id`),
-  CONSTRAINT `fk_user_id_event`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `meg`.`user` (`user_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 7
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  KEY `fk_project_id_idx` (`project_id`),
+  KEY `fk_project_id_event_idx` (`project_id`),
+  KEY `fk_user_id_event_idx` (`user_id`),
+  CONSTRAINT `fk_project_id_event` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`),
+  CONSTRAINT `fk_user_id_event` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `notification`
+--
 
--- -----------------------------------------------------
--- Table `meg`.`notification`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `meg`.`notification` ;
-
-CREATE TABLE IF NOT EXISTS `meg`.`notification` (
-  `notification_id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `message` VARCHAR(240) NOT NULL,
-  `read` TINYINT NOT NULL DEFAULT '0',
+DROP TABLE IF EXISTS `notification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notification` (
+  `notification_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `message` varchar(240) NOT NULL,
+  `read` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`notification_id`),
-  INDEX `fk_user_id_notification_idx` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_user_id_notification`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `meg`.`user` (`user_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  KEY `fk_user_id_notification_idx` (`user_id`),
+  CONSTRAINT `fk_user_id_notification` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `project`
+--
 
--- -----------------------------------------------------
--- Table `meg`.`works_on`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `meg`.`works_on` ;
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `project` (
+  `project_id` int NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`project_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `meg`.`works_on` (
-  `works_on_id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `project_id` INT NOT NULL,
-  `is_admin` TINYINT NOT NULL DEFAULT '0',
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `organization` varchar(255) DEFAULT NULL,
+  `password` char(60) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `works_on`
+--
+
+DROP TABLE IF EXISTS `works_on`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `works_on` (
+  `works_on_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `project_id` int NOT NULL,
+  `is_admin` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`works_on_id`),
-  INDEX `fk_project_id_idx` (`project_id` ASC) VISIBLE,
-  INDEX `fk_user_id_idx` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_project_id`
-    FOREIGN KEY (`project_id`)
-    REFERENCES `meg`.`project` (`project_id`),
-  CONSTRAINT `fk_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `meg`.`user` (`user_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  UNIQUE KEY `unique_proj_user` (`project_id`,`user_id`),
+  KEY `fk_project_id_idx` (`project_id`),
+  KEY `fk_user_id_idx` (`user_id`),
+  CONSTRAINT `fk_project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- -----------------------------------------------------
 -- STORED PROCEDURE TO ADD USER TO PROJECT &
